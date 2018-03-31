b046ec2ebd608bbacd07baee0d7b253040980dd1
sairam <dsairam17@gmail.com>
2018-03-31 18:45:59 +0530

    Added login functionalities

--- app/src/main/java/com/example/dsair/techfortechie/LoginActivity.java
+++ app/src/main/java/com/example/dsair/techfortechie/LoginActivity.java
@@ -2,12 +2,35 @@ package com.example.dsair.techfortechie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText mEmailEt, mPasswordEt;
+    String mLoginName, mLoginPass;
+    Button mLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mEmailEt = (EditText) findViewById(R.id.email);
+        mPasswordEt = (EditText) findViewById(R.id.password);
+        mLogin = (Button) findViewById(R.id.login_btn);
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogin();
+            }
+        });
+    }
+
+    public void userLogin(){
+        mLoginName = mEmailEt.getText().toString();
+        mLoginPass = mPasswordEt.getText().toString();
+        String action = "login";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(action, mLoginName, mLoginPass);
     }
 }
